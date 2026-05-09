3be3cc01220ba357e77aedd39b80af3f33ce207a
HiraFatima-ux <red.cherry7438@gmail.com>
2026-05-10 04:22:58 +0500

    Added CBoard class to chess.h

--- chess.h
+++ chess.h
@@ -1,1 +1,12 @@
+//Member 2
+class CBoard
+{
+public:
+    CBoard();
+    ~CBoard();
+    void Print();
+    bool IsInCheck(char PieceColor);
+    bool CanMove(char PieceColor);
+    GamePiece* MainGameBoard[8][8];
+};
 
