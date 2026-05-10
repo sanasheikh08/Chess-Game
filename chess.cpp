3ea85244cef1def1c2c6eb2b1b54a7516a0671c3
sanasheikh08 <sanasheikh19@outlook.com>
2026-05-10 12:46:03 +0500

    Implemented PawnPiece movement logic

--- chess.cpp
+++ chess.cpp
@@ -14,6 +14,37 @@ bool GamePiece::IsLegalMove(int iSrcRow, int iSrcCol, int iDestRow, int iDestCol
     }
     return false;
 }
+// =============================================
+//  PawnPiece
+// =============================================
+
+bool PawnPiece::AreSquaresLegal(int iSrcRow, int iSrcCol, int iDestRow, int iDestCol, GamePiece* GameBoard[8][8])
+{
+    GamePiece* qpDest = GameBoard[iDestRow][iDestCol];
+    if (qpDest == 0) {
+        if (iSrcCol == iDestCol) {
+            if (GetColor() == 'W') {
+                if (iDestRow == iSrcRow + 1) return true;
+                if (!mHasMoved && iDestRow == iSrcRow + 2 && GameBoard[iSrcRow + 1][iSrcCol] == 0) return true;
+            }
+            else {
+                if (iDestRow == iSrcRow - 1) return true;
+                if (!mHasMoved && iDestRow == iSrcRow - 2 && GameBoard[iSrcRow - 1][iSrcCol] == 0) return true;
+            }
+        }
+    }
+    else {
+        if ((iSrcCol == iDestCol + 1) || (iSrcCol == iDestCol - 1)) {
+            if (GetColor() == 'W') {
+                if (iDestRow == iSrcRow + 1) return true;
+            }
+            else {
+                if (iDestRow == iSrcRow - 1) return true;
+            }
+        }
+    }
+    return false;
+}
 //Member 2 code Part 
 //CBoard class constructor definition
 CBoard::CBoard()
